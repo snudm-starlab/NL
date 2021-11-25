841fee9140f2978c97148971f630ed3c883fe9e7
ant6si <50072144+ant6si@users.noreply.github.com>
2021-11-25 12:13:33 +0900

    Update train_NL.py

--- src/NL_Transformer/train_NL.py
+++ src/NL_Transformer/train_NL.py
@@ -3,12 +3,12 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
 """
 Legacy entry point. Use fairseq_cli/train.py or fairseq-train instead.
 """
 
 from fairseq_cli.train_NL import cli_main
-
 
 if __name__ == "__main__":
     cli_main()

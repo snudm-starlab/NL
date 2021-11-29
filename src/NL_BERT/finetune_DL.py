072e0787fc4704be775747518325c52ae254a875
ant6si <50072144+ant6si@users.noreply.github.com>
2021-11-29 14:56:52 +0900

    Update finetune_DL.py

--- src/NL_BERT/finetune_DL.py
+++ src/NL_BERT/finetune_DL.py
@@ -479,7 +479,6 @@ if args.do_train:
 #                     lr_this_step = args.learning_rate * warmup_linear(global_step / num_train_optimization_steps, args.warmup_proportion)
                     
 #                     for param_group in optimizer.param_groups:
-#                         param_group['lr'] = lr_this_step
                         
                 if (global_step+1) % 50 ==0:
                     print()

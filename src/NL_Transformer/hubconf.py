6d14fea75164361dbef9160df106e2ad3bf275b5
Hyojin Jeon <48641519+gywlssww@users.noreply.github.com>
2021-12-04 09:07:57 +0900

    Update hubconf.py

--- src/NL_Transformer/hubconf.py
+++ src/NL_Transformer/hubconf.py
@@ -2,7 +2,9 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-"""isort:skip_file"""
+
+"""isort:skip_file
+"""
 
 import functools
 import importlib
